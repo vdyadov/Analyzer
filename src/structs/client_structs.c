311797dd6354b0bb71da12956bd2f220fe4f5289
vdyadov <vdyadov@yandex.ru>
2021-11-20 17:31:43 +0700

    Исправил функции для работы со структурами

--- src/structs/client_structs.c
+++ src/structs/client_structs.c
@@ -1,41 +1,51 @@
 #include <client_structs.h>
 
-int create_packet_rtt(Packet_rtt *rtt_pack, int lenght)
+int create_packet_rtt(struct Packet_rtt *rtt_pack, int lenght)
 {
+    int ret = 0;
+
     rtt_pack->id_pack = ++id;
     rtt_pack->payload = malloc(size * sizeof(char));
 
     if (NULL == rtt_pack->payload)
     {
         perror("Malloc failed");
-        return 1;
+        ret = 1;
+        goto finally;
     }
 
     for (int i = 0; i < lenght; i++)
     {
-        rtt_pack->payload[i] = 65 + rand() % 60;
+        rtt_pack->payload[i] = i;
     }
-        
-    return 0;
+
+ finally:
+
+    return ret;
 }
 
-int create_packet_lose(Packet_lose *lose_pack, int round, int lenght)
+int create_packet_lose(struct Packet_lose *lose_pack, int round, int lenght)
 {
+    int ret = 0;
+    
     lose_pack->id_pack = ++id;
     lose_pack->id_round = round;
     lose_pack->payload = malloc(size * sizeof(char));
 
     if (NULL == lose_pack->payload)
-    {
+     {
         perror("Malloc failed");
-        return 1;
+        ret = 1;
+        goto finally;
     }
 
     for (int i = 0; i < lenght; i++)
     {
-        lose_pack->payload[i] = 65 + rand() % 60;
+        lose_pack->payload[i] = i;
     }
         
-    return 0;
+ finally:
+
+    return ret;
 }
 
--- src/structs/server_structs.c
+++ src/structs/server_structs.c
@@ -1,19 +1,42 @@
 #include <client_structs.h>
 
-int set_packet_rtt(Packet_rtt *rtt_pack, int id_pack, char *payload)
+int set_packet_rtt(Packet_rtt *rtt_pack, const int id_pack, char * const payload)
 {
+    int ret = 0;
+    
     rtt_pack->id_packet = id_pack;
+    
+    if (NULL == payload)
+    {
+        ret = 1;
+        goto finally;
+    }
+
     rtt_pack->payload = payload;
 
-    return 0;
+ finally:
+
+    return ret;
 }
 
-int set_packet_lose(Packet_lose *lose_pack, int id_round, int id_pack, char *payload)
+int set_packet_lose(Packet_lose *lose_pack, const int id_round, const int id_pack, 
+                    char * const payload)
 {
+    int ret = 0;
+    
     lose_pack->id_round = id_pack;
     lose_pack->id_packet = id_pack;
+    
+    if (NULL == payload)
+    {
+        ret = 1;
+        goto finally;
+    }
+
     lose_pack->payload = payload;
 
-    return 0;
+ finally:
+
+    return ret;
 }
 
